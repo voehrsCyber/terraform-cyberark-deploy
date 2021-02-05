27e0fe552ccc57b3fa49ad3b80fa8a981e211752
Jan-Patrick Vöhrs <jan-patrick.voehrs@cyberark.com>
2021-02-05 05:36:20 -0500

    Upload license file by using base64 string (missing update)

--- epv.tf
+++ epv.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+data "template_file" "userdata_win" {
+  template = <<EOF
+<powershell>
+net user ${var.INSTANCE_USERNAME} '${var.INSTANCE_PASSWORD}' /add /y
+net localgroup administrators ${var.INSTANCE_USERNAME} /add
+echo ${filebase64("./license.txt")} > tmp2.b64
+certutil -f -decode tmp2.b64 "C:/Program Files (x86)/PrivateArk/Server/Conf/License.xml"
+</powershell>
+EOF
+}
 provider "aws" {
   profile = "default"
   region  = "eu-central-1"
@@ -16,24 +26,11 @@ resource "aws_key_pair" "mykey" {
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-data "template_file" "userdata_win" {
-  template = <<EOF
-<script>
-echo "" > _INIT_STARTED_
-net user var.INSTANCE_USERNAME /add /y
-net user var.INSTANCE_USERNAME var.INSTANCE_PASSWORD
-net localgroup administrators var.INSTANCE_USERNAME /add
-echo "" > _INIT_COMPLETE_
-</script>
-<persist>false</persist>
-EOF
-}
-
 resource "aws_instance" "jpv-vault-12-terraform" {
   ami                    = "ami-04ac45703e289ef12"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.mykey.key_name
-  user_data              = data.template_file.userdata_win.rendered
+  user_data = data.template_file.userdata_win.rendered
   vpc_security_group_ids = [aws_security_group.allow-all.id]
   tags = {
     Name = "jpv-vault-12-terraform"
--- vars.tf
+++ vars.tf
@@ -1,4 +1,8 @@
 variable "PATH_TO_PRIVATE_KEY" { default = "mykey" }
 variable "PATH_TO_PUBLIC_KEY" { default = "mykey.pub" }
 variable "INSTANCE_USERNAME" { default = "admin" }
-variable "INSTANCE_PASSWORD" {}
+variable "INSTANCE_PASSWORD" {
+  type        = string
+  description = "This Password is used for a new local admin account"
+  default = "Cyberark1Cyberark!"
+}
